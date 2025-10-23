1abb14f6435d34c1752897c5c6f50bdc8d849675
mohammadtuhin0 <md.tuhin42525@gmail.com>
2025-10-23 12:02:22 +0600

    Add all C++ files

---
+++ Variables.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int x;
+    x= 10;
+    cout<<x;
+    // cout<<endl;
+    int y;
+    y=13;
+    cout<<y;
+    cout<<endl;
+    cout<<x+y;
+}
